697215838cd585550278fb11d62d7b0eddfd1024
FelipeMatoso <73952306+FelipeMatoso@users.noreply.github.com>
2020-11-24 14:04:18 -0300

    coment com seletor
    basicado no objeto p.completed, vc pode usar ele para fazer filtragem dos dados.

--- HTML-and-CSS-main/index-js/insere-bd.js
+++ HTML-and-CSS-main/index-js/insere-bd.js
@@ -5,60 +5,61 @@ click.addEventListener("click", function () {
     var xhr = new XMLHttpRequest();
 
     xhr.open("GET", "https://jsonplaceholder.typicode.com/todos");
-    console.log(xhr)
 
 
     xhr.addEventListener("load", function () {
         var pessoas = JSON.parse(xhr.responseText);
-        console.log(pessoas);
 
- 
+
 
         var tr = document.createElement("table");
 
         pessoas.forEach(p => {
-            
-       var userIdTr = document.querySelector(".userId");
-        var idTr = document.querySelector(".id");
-        var titleTr = document.querySelector(".title");
-        var completeTr = document.querySelector(".complete");
-
-        var userIdTd = document.createElement("td");
-        var idTd = document.createElement("td");
-        var titleTd = document.createElement("td");
-        var completeTd = document.createElement("td");
-
-         tr = document.createElement("tr");
-
-            userIdTr = p.userId;
-            idTr = p.id;
-            titleTr = p.title;
-            completeTr = p.completed;
-
-            userIdTd.textContent = p.userId;
-            userIdTd.classList.add("userId");
-            tr.appendChild(userIdTd, "userId");
 
 
-            idTd.textContent = p.id;
-            idTd.classList.add("id");
-            tr.appendChild(idTd, "id");
+                var userIdTr = document.querySelector(".userId");
+                var idTr = document.querySelector(".id");
+                var titleTr = document.querySelector(".title");
+                var completeTr = document.querySelector(".complete");
+
+                var userIdTd = document.createElement("td");
+                var idTd = document.createElement("td");
+                var titleTd = document.createElement("td");
+                var completeTd = document.createElement("td");
+
+                tr = document.createElement("tr");
+
+                userIdTr = p.userId;
+                idTr = p.id;
+                titleTr = p.title;
+                completeTr = p.completed;
 
 
+                userIdTd.textContent = p.userId;
+                userIdTd.classList.add("userId");
+                tr.appendChild(userIdTd, "userId");
 
-            titleTd.textContent = p.title;
-            titleTd.classList.add("title");
-            tr.appendChild(titleTd, "title");
+
+                idTd.textContent = p.id;
+                idTd.classList.add("id");
+                tr.appendChild(idTd, "id");
 
 
 
-            completeTd.textContent = p.completed;
-            completeTd.classList.add("completed");
-            tr.appendChild(completeTd, "complete");
+                titleTd.textContent = p.title;
+                titleTd.classList.add("title");
+                tr.appendChild(titleTd, "title");
 
-            tr.classList.add("tabela");
-            document.querySelector(".tabela").appendChild(tr);
-            console.log(tr);
+
+
+                completeTd.textContent = p.completed;
+                completeTd.classList.add("completed");
+                tr.appendChild(completeTd, "complete");
+
+                tr.classList.add("linha");
+                document.querySelector(".tabela").appendChild(tr);
+                console.log("feito");
+            }
 
         });
     })
